56a85df59e02e955257b2755ccd3e7b64f3050d0
Paweł Dawczak <pawel.dawczak@gmail.com>
2011-03-12 21:56:23 +0100

    sfDoctrineGuardPlugin installed and actived

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -9,5 +9,6 @@ class ProjectConfiguration extends sfProjectConfiguration
   public function setup()
   {
     $this->enablePlugins('sfDoctrinePlugin');
+    $this->enablePlugins('sfDoctrineGuardPlugin');
   }
 }
