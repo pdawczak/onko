99e1df323e9feebc0999e64d7bd9d2d5a63c72bc
Paweł Dawczak <pawel.dawczak@gmail.com>
2011-03-29 13:16:38 +0200

    function to format date added

--- lib/helper/myHelpersHelper.php
+++ lib/helper/myHelpersHelper.php
@@ -4,3 +4,14 @@ function myGetPartial($partial, $values)
 {
   return str_replace("\n", '', get_partial($partial, $values));
 }
+
+function myFormatDate($date)
+{
+  $time = strtotime($date);
+  return date('d.m.Y', $time);
+}
+
+function myBooleanWord($val)
+{
+  return $val ? 'Tak' : 'Nie';
+}
