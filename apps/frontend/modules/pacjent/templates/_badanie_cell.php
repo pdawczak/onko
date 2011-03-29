b575c82a0c753c90c35435b22355d13645795cf2
Paweł Dawczak <pawel.dawczak@gmail.com>
2011-03-29 13:15:45 +0200

    uses helper to format date

--- apps/frontend/modules/pacjent/templates/_badanie_cell.php
+++ apps/frontend/modules/pacjent/templates/_badanie_cell.php
@@ -1,6 +1,6 @@
 <div style="padding:10px;">
   <?php echo $i ?>.<br/>
-  Data badania: <?php echo $badanie->getDateTimeObject('data_badania')->format('d-m-Y') ?><br/>
+  Data badania: <?php echo myFormatDate($badanie->getDataBadania()) // $badanie->getDateTimeObject('data_badania')->format('d-m-Y') ?><br/>
   Ocena stanu guza: <?php //echo $badanie->getOcenaStanuGuzaSlowo() ?><br/>
   <?php echo link_to('Szczegóły', 'badanie_szczegoly', $badanie) ?>
 </div>
