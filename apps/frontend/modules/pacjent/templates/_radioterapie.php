a604a194ce855350642503ea4ff39b7b75e8387c
Paweł Dawczak <pawel.dawczak@gmail.com>
2011-03-29 13:20:47 +0200

    partial added

---
+++ apps/frontend/modules/pacjent/templates/_radioterapie.php
@@ -0,0 +1,32 @@
+<?php if (count($pacjent->getSortedRadioterapie()) > 0) : ?>
+  <!--<ul>-->
+  <?php foreach ($pacjent->getSortedRadioterapie() as $radioterapia) : ?>
+    <div class="cell-result-item">
+      <div class="result-item-date">
+        <?php echo myFormatDate($radioterapia->getDataRozpoczecia()) ?>
+        <?php if ($radioterapia->getDataZakonczenia()) : ?>
+          - <?php echo myFormatDate($radioterapia->getDataZakonczenia()) ?>
+        <?php endif ?>
+      </div>
+      <div class="result-item-result">
+        <ul>
+          <li>
+            fr./total:
+            <?php echo $radioterapia->getDawkaFr() ?> / <?php echo $radioterapia->getDawkaTotal() ?>
+          </li>
+          <li>
+            Stereo: <?php echo myBooleanWord($radioterapia->getStereo()) ?>
+          </li>
+          <li>
+            GTV/CTV/PTV:
+            <?php echo $radioterapia->getGtv() ?>/<?php echo $radioterapia->getCtv() ?>/<?php echo $radioterapia->getPtv() ?>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="cell-result-cleaner"></div>
+  <?php endforeach ?>
+  <!--</ul>-->
+<?php else : ?>
+  Brak radioterapii
+<?php endif ?>
