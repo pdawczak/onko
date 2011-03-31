60d166d6f3db9e4e336b4987cca90d544eb4675f
Paweł Dawczak <pawel.dawczak@gmail.com>
2011-04-01 01:19:27 +0200

    added myGetSortedChemioterapieForPacjent() method

--- lib/model/doctrine/ChemioterapiaTable.class.php
+++ lib/model/doctrine/ChemioterapiaTable.class.php
@@ -7,13 +7,31 @@
  */
 class ChemioterapiaTable extends Doctrine_Table
 {
-    /**
-     * Returns an instance of this class.
-     *
-     * @return object ChemioterapiaTable
-     */
-    public static function getInstance()
-    {
-        return Doctrine_Core::getTable('Chemioterapia');
-    }
+
+  /**
+   * Returns an instance of this class.
+   *
+   * @return ChemioterapiaTable
+   */
+  public static function getInstance()
+  {
+    return Doctrine_Core::getTable('Chemioterapia');
+  }
+
+  /**
+   *
+   * @param Pacjent $pacjent
+   * @return Doctrine_Collection
+   */
+  public function myGetSortedChemioterapieForPacjent(Pacjent $pacjent)
+  {
+    $q = $this
+      ->createQuery('c')
+      ->addWhere('c.pacjent_id = ?', $pacjent->getId())
+      ->orderBy('c.data_rozpoczecia DESC')
+    ;
+
+    return $q->execute();
+  }
+
 }
